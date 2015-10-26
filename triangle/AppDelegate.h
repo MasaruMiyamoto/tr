8abe5b2dcd861fa481a7b68ee7ac644ba90c7d36
MiyamotoMasaru <masaringaru@gmail.com>
2015-10-27 05:08:23 +0900

    Initial Commit

---
+++ triangle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  triangle
+//
+//  Created by miyamoto masaru on 2015/10/27.
+//  Copyright (c) 2015年 com.miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ triangle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  triangle
+//
+//  Created by miyamoto masaru on 2015/10/27.
+//  Copyright (c) 2015年 com.miyamoto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
